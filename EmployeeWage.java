3fd2216922881bf9b30c581b0c3a8d415eb6ca0b
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-08-17 22:28:53 +0530

    Welcome to Employee Wage

---
+++ EmployeeWage.java
@@ -0,0 +1,18 @@
+
+//Start with Displaying Welcome to Employee Wage Computation Program
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// print statement 
+		System.out.println("Welcome to Employee Wage Computation");
+	}
+
+}
+
+
+/*
+ * output 
+
+   Welcome to Employee Wage Computation
+ * **/
